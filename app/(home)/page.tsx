2cd35c27303bb45a3f347e8cfd120b94e3a6fe30
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-10-05 20:32:58 +0700

    Sign up & Sign in Authentication using Clerk

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <UserButton afterSignOutUrl="/sign-in"/>
+    </div>
   );
 }
