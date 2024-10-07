22850f7a3cf3a488a657e5fda45a614df70ad102
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-10-08 00:06:59 +0700

    Coding Edit course feature

--- app/(instructor)/instructor/courses/[courseId]/basic/page.tsx
+++ app/(instructor)/instructor/courses/[courseId]/basic/page.tsx
@@ -1,9 +1,64 @@
+import EditCourseForm from '@/components/courses/EditCourseForm'
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const CourseBasics = () => {
+const CourseBasics = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/sign-in')
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+      instructorId: userId,
+    }
+  })
+
+  if (!course) {
+    return redirect('/instructor/courses')
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      instructorId: userId
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: 'asc'
+    },
+    include: {
+      subCategories: true
+    }
+  })
+
+  const levels = await db.level.findMany({
+
+  })
+
   return (
-    <div>
-      
+    <div className='px-10'>
+      <EditCourseForm course={course} categories={categories.map((category) => ({
+        label: category.name,
+        value: category.id,
+        subCategories: category.subCategories.map((subCategory) => ({
+          label: subCategory.name,
+          value: subCategory.id,
+        }))
+      }))}
+        levels={levels.map((level) => ({
+          label: level.name,
+          value: level.id,
+        }))}
+      />
     </div>
   )
 }
---
+++ components/courses/EditCourseForm.tsx
@@ -0,0 +1,171 @@
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Course } from "@prisma/client"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import RichEditor from "@/components/custom/RichEditor"
+import { Combobox } from "@/components/custom/ComboBox"
+
+const formSchema = z.object({
+    title: z.string().min(2, {
+        message: "Title is required and must be at least 2 characters"
+    }),
+    subtitle: z.string().optional(),
+    description: z.string().optional(),
+    categoryId: z.string().min(1, {
+        message: "Category is required"
+    }),
+    subCategoryId: z.string().min(1, {
+        message: "Subcategory is required"
+    }),
+    levelId: z.string().optional(),
+    imageUrl: z.string().optional(),
+    price: z.coerce.number().optional(),
+})
+
+interface EditCourseFormProps {
+    course: Course;
+    categories: {
+        label: string; // name of the category
+        value: string; // category ID
+        subCategories: { label: string; value: string }[];
+    }[];
+    levels: { label: string; value: string }[];
+}
+
+const EditCourseForm = ({ course, categories, levels }: EditCourseFormProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: course.title,
+            subtitle: course.subtitle || "",
+            description: course.description || "",
+            categoryId: course.categoryId,
+            subCategoryId: course.subCategoryId,
+            levelId: course.levelId || "",
+            imageUrl: course.imageUrl || "",
+            price: course.price || undefined,
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Title</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Ex: Web Development for beginners" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="subtitle"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Subtitle</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Ex: Become a Full-stack with just ONE course. HTML, CSS, Javascript, Node, React, MongoDB and more" {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Description"</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Ex: Become a Full-stack with just ONE course. HTML, CSS, Javascript, Node, React, MongoDB and more" {...field} />
+                            </FormControl>
+                            <RichEditor placeholder="What is this course about?" {...field} />
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <div className="flex flex-wrap gap-10">
+
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col">
+                                <FormLabel>Category"</FormLabel>
+                                <FormControl>
+                                    <Combobox options={categories} {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="subCategoryId"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col">
+                                <FormLabel>Subcategory</FormLabel>
+                                <FormControl>
+                                    <Combobox
+                                        options={
+                                            categories.find(
+                                                (category) => category.value === form.watch("categoryId")
+                                            )?.subCategories || []
+                                        }
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="levelId"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col">
+                                <FormLabel>Level"</FormLabel>
+                                <FormControl>
+                                    <Combobox options={levels} {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default EditCourseForm
